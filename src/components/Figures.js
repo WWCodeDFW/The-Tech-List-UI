c41d4d47aa5ef75d31fe38504ded422105cd0037
Haley Demos <haleydemosother@gmail.com>
2019-10-24 00:24:05 -0500

    add Landing

--- src/components/Figures.js
+++ src/components/Figures.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const FiguresLanding = ({ figures }) => {
+const Figures = ({ figures }) => {
     return (
         <div>
-            <h1>The Tech List - Influential Figures in Tech</h1>
+
             {figures.map((figure) => (
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">{figure.name}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{figure.email}</h6>
+                        <h6 className="card-subtitle mb-2 text-muted">{figure.data}</h6>
                     </div>
                 </div>
             ))}
@@ -16,4 +17,4 @@ const FiguresLanding = ({ figures }) => {
     )
 };
 
-export default FiguresLanding
+export default Figures
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import Figures from './components/Figures.js';
+import Figures from './Figures.js';
 
 class App extends Component {
-    state = {
-        figures: []
-    };
 
-    componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/users')
-        //     fetch('http://localhost:3001/list')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ figures: data })
-            })
-            .catch(console.log)
-    }
     render() {
         return (
             <div className="App">
